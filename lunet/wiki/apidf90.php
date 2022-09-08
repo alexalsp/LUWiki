4282a8d6d15e9285060e8ca91b4c4d1ea46a8c9d
alexalsp <alex_trek_2007@list.ru>
2022-09-08 13:01:08 +0300

    Initial commit

---
+++ lunet/wiki/apidf90.php
@@ -0,0 +1,11 @@
+<?xml version="1.0"?>
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en">
+		<id>http://litcube.xtimelines.net/wiki/api.php?hidebots=1&amp;days=7&amp;limit=50&amp;action=feedrecentchanges&amp;feedformat=atom</id>
+		<title>Litcube's Universe  - Recent changes [en]</title>
+		<link rel="self" type="application/atom+xml" href="http://litcube.xtimelines.net/wiki/api.php?hidebots=1&amp;days=7&amp;limit=50&amp;action=feedrecentchanges&amp;feedformat=atom"/>
+		<link rel="alternate" type="text/html" href="http://litcube.xtimelines.net/wiki/index.php/Special:RecentChanges"/>
+		<updated>2022-08-29T12:49:49Z</updated>
+		<subtitle>Track the most recent changes to the wiki in this feed.</subtitle>
+		<generator>MediaWiki 1.26.0</generator>
+
+	</feed>
